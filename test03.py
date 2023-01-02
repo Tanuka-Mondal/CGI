eef4487ba7ced878eac3c15654691f7562001017
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2023-01-02 17:58:53 +0530

    Create test03.py

---
+++ test03.py
@@ -0,0 +1,23 @@
+#!C:\Users\tanumond\AppData\Local\Programs\Python\Python310\python.exe
+
+# Import modules for CGI handling 
+import cgi, cgitb 
+
+# Create instance of FieldStorage 
+form = cgi.FieldStorage() 
+
+# Get data from fields
+first_name = form.getvalue('first_name')
+last_name  = form.getvalue('last_name')
+
+
+
+print ("Content-type:text/html\r\n\r\n")
+print ("<html>")
+print ("<head>")
+print ("<title>Name</title>")
+print ("</head>")
+print ("<body>")
+print ("<h2>Hello %s %s</h2>" % (first_name, last_name))
+print ("</body>")
+print ("</html>")
