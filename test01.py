f542afb5c80651c2b0e3ea15f9139614c393a777
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2023-01-02 15:41:41 +0530

    hello world

---
+++ test01.py
@@ -0,0 +1,11 @@
+#!C:\Users\tanumond\AppData\Local\Programs\Python\Python310\python.exe
+
+print ("Content-type:text/html\r\n\r\n")
+print ('<html>')
+print ('<head>')
+print ('<title>Hello World - First CGI Program</title>')
+print ('</head>')
+print ('<body>')
+print ('<h2>Hello World! This is my first CGI program</h2>')
+print ('</body>')
+print ('</html>')
